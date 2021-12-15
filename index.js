a2de03ad34264b467ccc1115e1671aeb029f7932
williantian <williantian1995@gmail.com>
2021-12-15 20:12:48 +0800

    移除重复

--- index.js
+++ index.js
@@ -103,9 +103,5 @@ function askForAction (list, index) {
 module.exports.showAll = async () => {
 	// 读取之前的任务
 	const list = await db.read();
-	// 打印之前的任务
-	list.forEach((task, index) => {
-		console.log(`${task.done ? '[x]' : '[_]'} ${index + 1} - ${task.title}`)
-	});
 	printTasks(list)
 };
